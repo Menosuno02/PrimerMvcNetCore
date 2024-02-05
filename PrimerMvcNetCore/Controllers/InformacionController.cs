946857e57da7fc25ed6bad57ca71046b1b0d6246
Alejandro López Labajo <mainusone1@gmail.com>
2024-02-05 10:11:29 +0100

    Commit

---
+++ PrimerMvcNetCore/Controllers/InformacionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ControladorVista()
+        {
+            // Vamos a enviar información simple a nuestra vista
+            ViewData["NOMBRE"] = "Alumno";
+            ViewData["EDAD"] = 24;
+            ViewBag.DiaSemana = "Lunes";
+            ViewData["DIASEMANA"] = "Mañana martes";
+            return View();
+        }
+    }
+}
